6862b0f4f68f04ac2bf475a70f2f2619141f0094
Olga <pin007@tut.by>
2023-05-23 22:22:32 +0300

    #56 delete security

--- userservice/src/main/java/eu/senla/userservice/exception/ExceptionMessageConstant.java
+++ userservice/src/main/java/eu/senla/userservice/exception/ExceptionMessageConstant.java
@@ -4,6 +4,7 @@ public final class ExceptionMessageConstant {
 
     public static final String NOT_FOUND_USER = "User not found.";
     public static final String INVALID_TOKEN = "Invalid token.";
+    public static final String INVALID_PASSWORD = "Invalid password.";
     public static final String ADMIN_DONT_CREATE = "Admin don't create.";
     public static final String USER_EXIST = "User with such email exist.";
 }
