163c1802d3b79c042b0f600ec9495a66484a3e8b
Nikita Nasevich <nasevich.nik@mail.ru>
2023-05-30 13:55:59 +0300

    Tripservice: add kafka configuration

---
+++ tripservice/src/main/java/eu/senla/tripservice/util/KafkaTopicsName.java
@@ -0,0 +1,6 @@
+package eu.senla.tripservice.util;
+
+public class KafkaTopicsName {
+    public static final String NEW_FLIGHT_EVENT = "new_trip_event";
+    public static final String FLIGHT_CANCELED_EVENT = "trip_canceled_event";
+}
