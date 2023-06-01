a315b190aa64edbf7b2a0b88ef74d0b597aec4d2
Nikita Nasevich <nasevich.nik@mail.ru>
2023-06-01 12:19:18 +0300

    Tripservice: update configurations and ports

--- tripservice/src/main/java/eu/senla/tripservice/config/KafkaProducerConfig.java
+++ tripservice/src/main/java/eu/senla/tripservice/config/KafkaProducerConfig.java
@@ -1,8 +1,9 @@
 package eu.senla.tripservice.config;
 
 import eu.senla.tripservice.response.flight.KafkaFlightDTO;
+import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,8 +16,8 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
-@EnableKafka
 @Configuration
+@EnableKafka
 public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String kafkaServer;
@@ -25,7 +26,7 @@ public class KafkaProducerConfig {
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return props;
     }
@@ -33,6 +34,11 @@ public class KafkaProducerConfig {
     @Bean
     public ProducerFactory<String, Map<String, KafkaFlightDTO>> producerFactory() {
         return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public Producer<String, Map<String, KafkaFlightDTO>> producer() {
+        return producerFactory().createProducer();
     }
 
     @Bean
