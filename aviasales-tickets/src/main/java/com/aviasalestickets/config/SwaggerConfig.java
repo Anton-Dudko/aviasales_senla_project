1fc3996faf176cf596ec8d6dc98d30cabb711c4b
Anton <antondudko01@gmail.com>
2023-06-01 22:16:37 +0300

    change SwaggerConfig

--- aviasales-tickets/src/main/java/com/aviasalestickets/config/SwaggerConfig.java
+++ aviasales-tickets/src/main/java/com/aviasalestickets/config/SwaggerConfig.java
@@ -15,7 +15,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.aviasalestickeеs.controller"))
+                .apis(RequestHandlerSelectors.basePackage("com.aviasalestickets.controller"))
                 .paths(PathSelectors.any())
                 .build();
     }
