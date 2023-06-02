ddffe8854df388cbb683ce28987374272ada9e7a
Olga <pin007@tut.by>
2023-06-02 12:54:59 +0300

    topic
    # Conflicts:
    #	userservice/src/main/java/eu/senla/userservice/kafka/KafkaTopicConstant.java

--- userservice/src/main/java/eu/senla/userservice/kafka/KafkaTopicConstants.java
+++ userservice/src/main/java/eu/senla/userservice/kafka/KafkaTopicConstants.java
@@ -3,5 +3,5 @@ package eu.senla.userservice.kafka;
 public class KafkaTopicConstants {
     public static final String REGISTERED_EVENT = "user_registered_event";
     public static final String RESET_PASSWORD_EVENT = "user_reset_password_event";
-    public static final String UPDATE_PASSWORD_EVENT = "password_update_event";
+    public static final String UPDATE_PASSWORD_EVENT = "user_update_password_event";
 }
