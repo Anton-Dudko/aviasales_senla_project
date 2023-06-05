6809229564ed02dd38185309e5d1b78825800d8c
Anton <antondudko01@gmail.com>
2023-06-05 22:00:58 +0300

    change files for kafka

--- aviasales-tickets/src/main/java/com/aviasalestickets/config/KafkaProducerConfig.java
+++ aviasales-tickets/src/main/java/com/aviasalestickets/config/KafkaProducerConfig.java
@@ -1,10 +1,11 @@
 package com.aviasalestickets.config;
 
+import com.aviasalestickets.config.properties.KafkaProperties;
 import com.aviasalestickets.model.dto.KafkaTicketDto;
+import lombok.RequiredArgsConstructor;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -16,17 +17,17 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 import java.util.HashMap;
 import java.util.Map;
 
+
 @Configuration
 @EnableKafka
+@RequiredArgsConstructor
 public class KafkaProducerConfig {
 
-    @Value("${spring.kafka.bootstrap-servers}")
-    private String bootstrapServers;
-
+    private final KafkaProperties kafkaProperties;
 
     public Map<String, Object> producerConfigs() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProperties.getBootstrapServers());
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return props;
