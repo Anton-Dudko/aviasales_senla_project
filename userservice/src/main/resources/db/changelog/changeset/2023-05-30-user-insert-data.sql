f0ef45540846ea84b58e19c152cfd28a70510b97
Glavdel Olga <pin007@tut.by>
2023-06-08 09:52:54 +0000

    userservice: fix update bug

--- userservice/src/main/resources/db/changelog/changeset/2023-05-30-user-insert-data.sql
+++ userservice/src/main/resources/db/changelog/changeset/2023-05-30-user-insert-data.sql
@@ -1,10 +1,10 @@
 INSERT INTO users (username, email, password, date_birth, language, role, refresh_token, access_token)
-VALUES ('admin', 'pin007@tut.by', '$2a$13$tj80en8/NPWEiQBLXTUnwOhQ4b89MQl6zxXa31yujN2TVDoAJtm2q', '2012-12-12', 'RU',
-        'ROLE_ADMIN',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY4ODczOTU2MX0.qlFeqIj0dl7PglScIMWoDoB49zEvA6jVP6OI7zUm9R-IXiBbPkkFSIDG7UWAstQTVUYOQ8O89gEuHomYVDWaAw',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY4NzIyNzU2MCwicm9sZSI6IlJPTEVfQURNSU4ifQ.wZw8URhNFo-X62f3h3xm9BK_U2p3KQA1ssDzMKeb1J0zn3att-_NLNLiYzzrgz6dkqWkQEsNKTgZ_gTet2-FuQ');
+VALUES ('OlgaAdmin', 'pin007@tut.by', '$2a$13$cAb/HetuB7bvLX7ZAR3U.u06idizYkU4.PtgIkHpdGBRvcVnpUMAW', '2009-09-09',
+        'EN', 'ROLE_ADMIN',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaW4wMDdAdHV0LmJ5IiwiZXhwIjoxNjg4ODA3MDQwfQ.edpQFMeF3LefdjghDeZbqpjN4NNFGjatXqR0UANXDq2RXgWiL04nb4oW4x7dbxtzkWHbOqK1PKvRDfN_9DD4xQ',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaW4wMDdAdHV0LmJ5IiwiZXhwIjoxNjg2MzAxNDQwLCJyb2xlIjoiUk9MRV9BRE1JTiJ9.lIxKi6CDv4_lbb_-cPsZKEGtMfDENlBQxuQJsDpjaW5-u-LweMneosFhR8tbLFj36_RaWtlgCCTBl6lGuliVAA');
 INSERT INTO users (username, email, password, date_birth, language, role, refresh_token, access_token)
-VALUES ('user', 'olga.glavdel@gmail.com', '$2a$13$GtpQAk5.XdfAQKpcWHG2aO5muFqL.PWl5H.z3vCXITE4eCRulS3KO', '2012-12-12',
-        'EN', 'ROLE_USER',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg4NzM5NTY0fQ.DZijdtFr_11V7IuWYqpQodKdzJzLI2QR0KAg__kkceArSyMNSWNtw2VZ__a5xQP_T-XMnkkr-BT9QQCRxG4AoA',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNjg3MjI3NTY0LCJyb2xlIjoiUk9MRV9VU0VSIn0.e7lrhte6BUy06eggtA5ITxsaewlyWvf_SZASZJHDPkhhK8v6_b18Kgi0k8O0xSN93q0yQF-86qF46VEn00vMGg');
+VALUES ('UserName11', 'olga.glavdel@gmail.com', '$2a$13$jNYMTb1bido0TogIZf6KZeN3TBy7q4wZ6TT6AMwz5YRYO3M5oBx.2',
+        '2012-12-12', 'RU', 'ROLE_USER',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvbGdhLmdsYXZkZWxAZ21haWwuY29tIiwiZXhwIjoxNjg4ODA3MDg5fQ.R3QsTJGb7J26i3uWRAe8KYXTVOY7NvHxdJR4Y2CBCSatZP_jNLT5sEJPJf5v-mc5x3Rwsqut8Mq8YvXoATWWHA',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvbGdhLmdsYXZkZWxAZ21haWwuY29tIiwiZXhwIjoxNjg2MzAxNDg5LCJyb2xlIjoiUk9MRV9VU0VSIn0.ch4WsgaNN1Zd-TAQJWOSWpykULfwAtMCBNGNz0RJ3W1y6I5ZylbBMxz5CwmS6qerq7dgcJmFqFYHi_bVQ4u6_w');
