d26c079b25f5777d48c9ed28a04a13352b50f2d9
Glavdel Olga <pin007@tut.by>
2023-06-08 11:34:13 +0000

    userservice: add user for testing another services

--- userservice/src/main/resources/db/changelog/changeset/2023-05-30-user-insert-data.sql
+++ userservice/src/main/resources/db/changelog/changeset/2023-05-30-user-insert-data.sql
@@ -1,10 +1,23 @@
 INSERT INTO users (username, email, password, date_birth, language, role, refresh_token, access_token)
-VALUES ('OlgaAdmin', 'pin007@tut.by', '$2a$13$cAb/HetuB7bvLX7ZAR3U.u06idizYkU4.PtgIkHpdGBRvcVnpUMAW', '2009-09-09',
+VALUES ('OlgaAdmin', 'pin007@tut.by', '$2a$13$agzkNNVHksw1m6MrLRiUJexv50XmRkvWUZ9GKtQtlyGuM6KH3EmI2', '2009-09-09',
         'EN', 'ROLE_ADMIN',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaW4wMDdAdHV0LmJ5IiwiZXhwIjoxNjg4ODA3MDQwfQ.edpQFMeF3LefdjghDeZbqpjN4NNFGjatXqR0UANXDq2RXgWiL04nb4oW4x7dbxtzkWHbOqK1PKvRDfN_9DD4xQ',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaW4wMDdAdHV0LmJ5IiwiZXhwIjoxNjg2MzAxNDQwLCJyb2xlIjoiUk9MRV9BRE1JTiJ9.lIxKi6CDv4_lbb_-cPsZKEGtMfDENlBQxuQJsDpjaW5-u-LweMneosFhR8tbLFj36_RaWtlgCCTBl6lGuliVAA');
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaW4wMDdAdHV0LmJ5IiwiZXhwIjoxNjg4ODE1MTQ1fQ.cVIdSIa63Clvbo3kCP-F4FV6XJ6KA6g1aM5eG_Yvm1lx749VOfOJyLWZkLyJKG-x445O7ZB1-h6DaKGrfSewVQ',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJwaW4wMDdAdHV0LmJ5IiwiZXhwIjoxNjg2MzA5NTQ1LCJyb2xlIjoiUk9MRV9BRE1JTiJ9.e609r83sayyY-5wzLgIw43E7l1rdQkJqbUcltAqG4_f8G9XKkkqQonl8xuJTle8mhQ498OLL4htdEZak6wlF-w');
+
 INSERT INTO users (username, email, password, date_birth, language, role, refresh_token, access_token)
-VALUES ('UserName11', 'olga.glavdel@gmail.com', '$2a$13$jNYMTb1bido0TogIZf6KZeN3TBy7q4wZ6TT6AMwz5YRYO3M5oBx.2',
+VALUES ('UserName11', 'olga.glavdel@gmail.com', '$2a$13$MUAdkSqDowYPidowl3w.1uSf0fxElYePqBbmdA8rVyi49KvJCzDb2',
         '2012-12-12', 'RU', 'ROLE_USER',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvbGdhLmdsYXZkZWxAZ21haWwuY29tIiwiZXhwIjoxNjg4ODA3MDg5fQ.R3QsTJGb7J26i3uWRAe8KYXTVOY7NvHxdJR4Y2CBCSatZP_jNLT5sEJPJf5v-mc5x3Rwsqut8Mq8YvXoATWWHA',
-        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvbGdhLmdsYXZkZWxAZ21haWwuY29tIiwiZXhwIjoxNjg2MzAxNDg5LCJyb2xlIjoiUk9MRV9VU0VSIn0.ch4WsgaNN1Zd-TAQJWOSWpykULfwAtMCBNGNz0RJ3W1y6I5ZylbBMxz5CwmS6qerq7dgcJmFqFYHi_bVQ4u6_w');
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvbGdhLmdsYXZkZWxAZ21haWwuY29tIiwiZXhwIjoxNjg4ODE1MTU0fQ.sW4cdJRkifySEqC6YVzBptReN-U3AvB3HTBcdVlQiliYC-xofbOfH4Z08Ai-a7yYlkLu1qwqGzRxBh6Xs-rpZw',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvbGdhLmdsYXZkZWxAZ21haWwuY29tIiwiZXhwIjoxNjg2MzA5NTU0LCJyb2xlIjoiUk9MRV9VU0VSIn0.UfWJhN-MO8R8Kc_52aWufzYgpEAnmrB1SLFe6B_4QYKKG-BDwbI-orw8pkS6-zwNKrtVJcscSdOfCAPEWjoLdQ');
+
+INSERT INTO users (username, email, password, date_birth, language, role, refresh_token, access_token)
+VALUES ('username2', 'someemail@gmaaaail.com', '$2a$13$g84ytPF0k.njXnU2q2Clx.8sOTLOVyDoKjOTGi22idgmK95WNF5Wm',
+        '1980-05-10', 'EN', 'ROLE_USER',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzb21lZW1haWxAZ21hYWFhaWwuY29tIiwiZXhwIjoxNjg4ODE1MTg3fQ.dYPw221C7jLkWTdMK9D5GOrHwQv0hhf67flGpSq5vUIyWw6nj6xTdGFh2vEVQRpOMdh9kt898qiA8LlPsu5HTQ',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzb21lZW1haWxAZ21hYWFhaWwuY29tIiwiZXhwIjoxNjg2MzA5NTg3LCJyb2xlIjoiUk9MRV9VU0VSIn0.SOoetBTwHKjJEZtTI1wnGlvoW2lDZW-UN6E8jFeNDEXPQQYnf75qUzcki9ZQThPbrcDJhmvbnExrudkgA9iTzQ');
+
+INSERT INTO users (username, email, password, date_birth, language, role, refresh_token, access_token)
+VALUES ('username3', 'someemail@gmaaaaail.com', '$2a$13$XezENL5/oGwt449a.F.0BOx4Rcnoe5K7ljsi18ilWVkt1IKjVAraK',
+        '1999-07-13', 'EN', 'ROLE_USER',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzb21lZW1haWxAZ21hYWFhYWlsLmNvbSIsImV4cCI6MTY4ODgxNTIxNH0.tNslgNBX7CFSXnb3j57ax8xWt6a5IgAeG8T68p5J7Vdd_F-IAVCej5jPvY1qoILFDDtUjQSDfe9RBaSaYh_uoA',
+        'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzb21lZW1haWxAZ21hYWFhYWlsLmNvbSIsImV4cCI6MTY4NjMwOTYxNCwicm9sZSI6IlJPTEVfVVNFUiJ9.Tgotphde9KsFkYtdnVwDfreW04DuvXQy3UaXvPjKVY2DL8n32_8Tl9YX8wc58ElS3VqqmxrDGAJp5BUEzc5iOA');
